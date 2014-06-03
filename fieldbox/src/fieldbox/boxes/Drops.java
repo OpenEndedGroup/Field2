87fa1384c1f28e8c86c2eeecb2a34015ca450aa8
marc <marc@d0.local>
2014-06-02 20:45:33 -0500

    initial drag and drop support

---
+++ fieldbox/src/fieldbox/boxes/Drops.java
@@ -0,0 +1,25 @@
+package fieldbox.boxes;
+
+import field.graphics.Window;
+import field.utility.Dict;
+
+import java.util.Collection;
+
+/**
+ * Entry-point for File drops from GLFW into Field
+ */
+public class Drops {
+
+	public interface OnDrop
+	{
+		public void onDrop(Window.Event<Window.Drop> drop);
+	}
+
+	static public final Dict.Prop<Collection<OnDrop>> onDrop = new Dict.Prop<>("onDrop").type().toCannon();
+
+	public void dispatch(Box root, Window.Event<Window.Drop> drop)
+	{
+
+	}
+
+}
