428127f7d225035d7e128e805ea2b84a2d634700
marc downie <marc@openendedgroup.com>
2014-06-02 20:38:00 -0500

    vastly better import completion and completion for java statics

--- fieldnashorn/src/fieldnashorn/JavaSupport.java
+++ fieldnashorn/src/fieldnashorn/JavaSupport.java
@@ -5,43 +5,62 @@ import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaField;
 import com.thoughtworks.qdox.model.JavaMethod;
 import com.thoughtworks.qdox.parser.ParseException;
+import field.graphics.RunLoop;
 import field.utility.Pair;
 import fielded.Execution;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.jar.JarFile;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * When Nashorn/Javascript completion bumps up against something that's actually a Java object, we can use Java reflection based completion. Better
  * yet, if we have the source code fore that Java we can get very good completion information with parameter names, generics and javadocs. We're using
  * qdox to parse the Java sources.
  *
+ * This class also contains our class discovery mechanism for import help
+ * <p>
  * Todo: this is useful accross language runtimes, not just Nashorn/Javascript.
  */
 public class JavaSupport {
-
 
 	public interface HandlesCompletion {
 		public List<Execution.Completion> getCompletionsFor(String prefix);
 	}
 
-
 	private final JavaProjectBuilder builder;
+
+	Map<String, String> allClassNames = new LinkedHashMap<>();
 
 	public JavaSupport() {
 		builder = new JavaProjectBuilder();
 
-
-		System.out.println(" inside java support constructor ");
-
 		List<Path> all = new ArrayList<>();
 
-		new Thread(() -> {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+		if (classLoader instanceof URLClassLoader) {
+			URL[] paths = ((URLClassLoader) classLoader).getURLs();
+			for (URL path : paths) {
+				RunLoop.workerPool.submit(() -> {
+					Map<String, String> a = indexClasses(path);
+					synchronized (allClassNames) {
+						allClassNames.putAll(a);
+					}
+				});
+			}
+
+		}
+
+		RunLoop.workerPool.submit(() -> {
 			try {
 				String root = "/home/marc/fieldwork2/";
 				Files.walkFileTree(FileSystems.getDefault().getPath(root), new FileVisitor<Path>() {
@@ -78,9 +97,32 @@ public class JavaSupport {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}).start();
+		});
 		System.out.println(" all is :" + all);
 
+	}
+
+	private Map<String, String> indexClasses(URL path) {
+		String f = path.getFile();
+		if (f.endsWith(".jar")) return indexClasses_jar(f);
+		else if (new File(f).exists()) return indexClasses_tree(f);
+		return Collections.emptyMap();
+	}
+
+	// TODO: not implemented yet
+	private Map<String, String> indexClasses_tree(String f) {
+		return Collections.emptyMap();
+
+	}
+
+	private Map<String, String> indexClasses_jar(String f) {
+		try {
+			return new JarFile(f).stream().filter(e -> e.getName().endsWith(".class"))
+				    .map(e -> e.getName().replace('/', '.').replace(".class", "")).collect(Collectors.toMap(x -> x, x -> f));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Collections.emptyMap();
 	}
 
 	public List<Execution.Completion> getCompletionsFor(Object o, String prefix) {
@@ -88,20 +130,24 @@ public class JavaSupport {
 			return ((HandlesCompletion) o).getCompletionsFor(prefix);
 		}
 
+		boolean staticsOnly = o instanceof Class;
+
 		Class c = o instanceof Class ? (Class) o : o.getClass();
 
 		JavaClass j = builder.getClassByName(c.getName());
 
+		System.out.println(" java class (for javadoc supported completion) :"+j);
+
 		List<Execution.Completion> r = new ArrayList<>();
 		for (JavaField m : j.getFields()) {
-			if (m.getName().startsWith(prefix) && m.getModifiers().contains("public")) {
+			if (m.getName().startsWith(prefix) && m.getModifiers().contains("public") && (!staticsOnly || m.getModifiers().contains("static"))) {
 				r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m.getName(), m
 					    .getDeclarationSignature(true)) + "</span>" + (m.getComment() != null ? "&mdash; <span class=doc>" + m
 					    .getComment() + "</span>" : "")));
 			}
 		}
 		for (JavaMethod m : j.getMethods()) {
-			if (m.getName().startsWith(prefix) && m.getModifiers().contains("public")) {
+			if (m.getName().startsWith(prefix) && m.getModifiers().contains("public") && (!staticsOnly || m.getModifiers().contains("static"))) {
 				r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m.getName(), m
 					    .getDeclarationSignature(true)) + "</span>" + (m.getComment() != null ? "&mdash; <span class=doc>" + m
 					    .getComment() + "</span>" : "")));
@@ -144,12 +190,27 @@ public class JavaSupport {
 
 	public List<Pair<String, String>> getPossibleJavaClassesFor(String left) {
 		List<Pair<String, String>> rr = new ArrayList<>();
+
+		Set<String> seen = new LinkedHashSet<>();
+
 		for (JavaClass c : builder.getClasses()) {
-			System.out.println(" javaclass :" + c.getFullyQualifiedName());
-			if (c.getName().contains(left)) {
+			if (c.getName().contains(left) && !seen.contains(c.getFullyQualifiedName())) {
+				seen.add(c.getFullyQualifiedName());
 				rr.add(new Pair<>(c.getFullyQualifiedName(), c.getComment()));
 			}
 		}
+
+		synchronized (allClassNames) {
+			for (Map.Entry<String, String> e : allClassNames.entrySet()) {
+				if (e.getKey().contains(left) && !seen.contains(e.getKey())) {
+					seen.add(e.getKey());
+					rr.add(new Pair<>(e.getKey(), "from "+e.getValue()));
+				}
+			}
+		}
+
+		if (rr.size()>100) rr = rr.subList(0, 100);
+
 		return rr;
 	}
 
