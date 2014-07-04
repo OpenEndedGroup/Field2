a14292275f3d3f7601ca807ba0771002048a9160
marc <marc@openendedgroup.com>
2014-07-04 15:45:28 -0500

    Fixed 'n' to create new element

--- fieldbox/src/fieldbox/DefaultMenus.java
+++ fieldbox/src/fieldbox/DefaultMenus.java
@@ -54,7 +54,6 @@ public class DefaultMenus extends Box {
 		});
 
 		properties.putToList(Keyboard.onKeyDown, (e, k) -> {
-			System.out.println(" key down :"+e+" "+k+" "+Glfw.GLFW_KEY_N);
 			if ( k == Glfw.GLFW_KEY_N)
 			{
 				newBox(convertCoordinateSystem(e.after.mouseState), root);
