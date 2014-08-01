05af18ffe3b8b1375990a69891c46466bde4813e
marc <marc@openendedgroup>
2014-07-31 22:38:48 -0500

    JavaSupport now supports annotations to fine tune completions on classes

--- fieldnashorn/src/fieldnashorn/JavaSupport.java
+++ fieldnashorn/src/fieldnashorn/JavaSupport.java
@@ -1,7 +1,7 @@
 package fieldnashorn;
 
 import com.thoughtworks.qdox.JavaProjectBuilder;
-import com.thoughtworks.qdox.directorywalker.*;
+import com.thoughtworks.qdox.model.JavaAnnotation;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaField;
 import com.thoughtworks.qdox.model.JavaMethod;
@@ -10,13 +10,15 @@ import field.graphics.RunLoop;
 import field.utility.Log;
 import field.utility.Pair;
 import fielded.Execution;
+import fieldnashorn.annotations.HiddenInAutocomplete;
+import fieldnashorn.annotations.JavaDocOnly;
+import fieldnashorn.annotations.StopAutocompleteHere;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.*;
-import java.nio.file.FileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.jar.JarFile;
@@ -28,7 +30,7 @@ import java.util.stream.Collectors;
  * When Nashorn/Javascript completion bumps up against something that's actually a Java object, we can use Java reflection based completion. Better
  * yet, if we have the source code fore that Java we can get very good completion information with parameter names, generics and javadocs. We're using
  * qdox to parse the Java sources.
- *
+ * <p>
  * This class also contains our class discovery mechanism for import help
  * <p>
  * Todo: this is useful accross language runtimes, not just Nashorn/Javascript.
@@ -66,7 +68,8 @@ public class JavaSupport {
 					});
 				}
 
-				builder.setErrorHandler( e -> Log.log("completion.general", " problem parsing Java source file for completion, will skip this file and continue on "));
+				builder.setErrorHandler(e -> Log
+					    .log("completion.general", " problem parsing Java source file for completion, will skip this file and continue on "));
 				builder.addClassLoader(classLoader);
 
 				String root = fieldagent.Main.app;
@@ -75,10 +78,11 @@ public class JavaSupport {
 					public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 						if (dir.endsWith("temp")) return FileVisitResult.SKIP_SUBTREE;
 						if (dir.endsWith("src")) {
-							Log.log("completion.debug"," added " + dir + " to source path");
+							Log.log("completion.debug", " added " + dir + " to source path");
 							all.add(dir);
 							try {
-								builder.addSourceTree(dir.toFile(), f -> Log.log("completion.error", " error parsing file "+f+", but we'll continue on anyway..."));
+								builder.addSourceTree(dir.toFile(), f -> Log
+									    .log("completion.error", " error parsing file " + f + ", but we'll continue on anyway..."));
 							} catch (ParseException e) {
 								e.printStackTrace();
 							}
@@ -138,9 +142,7 @@ public class JavaSupport {
 			Log.log("completion.debug", " object :" + o + " is a completion handler ");
 
 			return ((HandlesCompletion) o).getCompletionsFor(prefix);
-		}
-		else
-			Log.log("completion.debug", " object :"+o+" is not a completion handler ");
+		} else Log.log("completion.debug", " object :" + o + " is not a completion handler ");
 
 		boolean staticsOnly = o instanceof Class;
 
@@ -150,45 +152,61 @@ public class JavaSupport {
 
 		JavaClass j = builder.getClassByName(c.getName());
 
-		Log.log("completion.debug", " java class (for javadoc supported completion) :"+j+" prefix is <"+prefix+">");
+		Log.log("completion.debug", " java class (for javadoc supported completion) :" + j + " prefix is <" + prefix + ">");
 
 		List<Execution.Completion> r = new ArrayList<>();
 		try {
-			while(j!=null) {
-				for (JavaField m : j.getFields()) {
-					if ((prefix.equals("") || m.getName().startsWith(prefix)) && m.getModifiers().contains("public") && (!staticsOnly || m.getModifiers()
-						    .contains("static"))) {
-						r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m.getName(), m
-							    .getDeclarationSignature(true)) + "</span>" + (m.getComment() != null ? "<span class=type>&nbsp;&mdash;</span> <span class=doc>" + m.getComment() + "</span>" : "")));
-					}
-				}
-				for (JavaMethod m : j.getMethods()) {
-					if ((prefix.equals("") ||m.getName().startsWith(prefix)) && m.getModifiers().contains("public") && (!staticsOnly || m.getModifiers()
-						    .contains("static"))) {
-						r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m.getName(), m
-							    .getDeclarationSignature(true)) + "</span>" + (m.getComment() != null ? "<span class=type>&nbsp;&mdash;</span> <span class=doc>" + m.getComment() + "</span>" : "")));
-					}
-				}
+			while (j != null) {
 
+				boolean docOnly = c.getAnnotation(JavaDocOnly.class) != null;
+				if (c.getAnnotation(StopAutocompleteHere.class) != null) break;
+
+				if (c.getAnnotation(HiddenInAutocomplete.class) == null) {
+
+					for (JavaField m : j.getFields()) {
+						if (hasAnnotation(m.getAnnotations(), HiddenInAutocomplete.class)) continue;
+						if (docOnly && m.getComment().trim().length() < 1) continue;
+						if ((prefix.equals("") || m.getName().startsWith(prefix)) && m.getModifiers()
+							    .contains("public") && (!staticsOnly || m.getModifiers().contains("static"))) {
+							r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m
+								    .getName(), m.getDeclarationSignature(true)) + "</span>" + (m
+								    .getComment() != null ? "<span class=type>&nbsp;&mdash;</span> <span class=doc>" + m
+								    .getComment() + "</span>" : "")));
+						}
+					}
+					for (JavaMethod m : j.getMethods()) {
+						if (hasAnnotation(m.getAnnotations(), HiddenInAutocomplete.class)) continue;
+						if (docOnly && m.getComment().trim().length() < 1) continue;
+						if ((prefix.equals("") || m.getName().startsWith(prefix)) && m.getModifiers()
+							    .contains("public") && (!staticsOnly || m.getModifiers().contains("static"))) {
+							r.add(new Execution.Completion(-1, -1, m.getName(), "<span class=type>" + compress(m
+								    .getName(), m.getDeclarationSignature(true)) + "</span>" + (m
+								    .getComment() != null ? "<span class=type>&nbsp;&mdash;</span> <span class=doc>" + m
+								    .getComment() + "</span>" : "")));
+						}
+					}
+				}
 				c = c.getSuperclass();
-				if (c==null) break;
+				if (c == null) break;
 				j = builder.getClassByName(c.getName());
 
 				//TODO: should we stop when we get into java.* classes if we started from something that wasnt?
 				// PApplet's completion is heavily polluted by this stuff.
 				// let's try that now.
 
-				boolean isJava = c.getName().startsWith("java");
+				boolean isJava = c.getName().startsWith("java") || c.getName().startsWith("jdk");
 				if (!wasJava && isJava) break;
 
 			}
-		}
-		catch(Throwable t)
-		{
+		} catch (Throwable t) {
 			t.printStackTrace();
 		}
 
 		return r;
+	}
+
+	private boolean hasAnnotation(List<JavaAnnotation> annotations, Class hiddenInAutocompleteClass) {
+		return annotations.stream().filter(x -> x.getType().getName().equals(hiddenInAutocompleteClass.getName().substring(1+hiddenInAutocompleteClass.getName().lastIndexOf('.')))).findFirst().isPresent();
 	}
 
 	static public String compress(String name, String signature) {
@@ -234,12 +252,12 @@ public class JavaSupport {
 			for (Map.Entry<String, String> e : allClassNames.entrySet()) {
 				if (e.getKey().contains(left) && !seen.contains(e.getKey())) {
 					seen.add(e.getKey());
-					rr.add(new Pair<>(e.getKey(), "from "+e.getValue()));
+					rr.add(new Pair<>(e.getKey(), "from " + e.getValue()));
 				}
 			}
 		}
 
-		if (rr.size()>100) rr = rr.subList(0, 100);
+		if (rr.size() > 100) rr = rr.subList(0, 100);
 
 		return rr;
 	}
