ab16f5535e18043dea2e06c37fe3cb4ff2e85b9d
Peter Kang <kangp3@rpi.edu>
2014-08-07 12:37:48 -0400

    Added click events to show the colorPicker

--- fielded/internal/colorPicker.js
+++ fielded/internal/colorPicker.js
@@ -1,10 +1,11 @@
 function addColorPicker() {
-		var pos = cm.getCursor()
-		var li = cm.lineInfo(pos.line)
-		console.log(pos)
-    var d = $('<div class="'+li.textClass+'"><form><input type="text" id="color'+colorpickernum+'" name="color" value="#000000" /></form><div id="colorpicker'+colorpickernum+'"></div></div>')[0]
-		bm = cm.addLineWidget(pos.line, d, { handleMouseEvents: true })
-		$(document).ready(function() {
-        $.farbtastic('#colorpicker'+colorpickernum, '#color'+colorpickernum);
-    });
+	var pos = cm.getCursor()
+	var li = cm.lineInfo(pos.line)
+	var d = $('<div class="rm_color"><form><input type="text" id="color'+colorpickernum+'" name="color" value="#000000" /></form><div class="colorpicker" id="colorpicker'+colorpickernum+'"></div></div>')[0]
+	bm = cm.addLineWidget(pos.line, d, { })
+	$(".rm_color").click(function() {
+  	var placeholder = $(this).find('.colorpicker');
+  	var input = $(this).find('input[type=text]');
+  	$.farbtastic(placeholder,input);
+  });
 }
