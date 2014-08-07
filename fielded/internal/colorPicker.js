dcb1cbb4ea1c7390ba65bf97fdc0ba1cd52879fc
Peter Kang <kangp3@rpi.edu>
2014-08-07 15:23:15 -0400

    Finished colorpicker implementation, press ctrl-Q while color to change is selected

--- fielded/internal/colorPicker.js
+++ fielded/internal/colorPicker.js
@@ -1,11 +1,16 @@
 function addColorPicker() {
-	var pos = cm.getCursor()
-	var li = cm.lineInfo(pos.line)
-	var d = $('<div class="rm_color"><form><input type="text" id="color'+colorpickernum+'" name="color" value="#000000" /></form><div class="colorpicker" id="colorpicker'+colorpickernum+'"></div></div>')[0]
-	bm = cm.addLineWidget(pos.line, d, { })
-	$(".rm_color").click(function() {
-  	var placeholder = $(this).find('.colorpicker');
-  	var input = $(this).find('input[type=text]');
-  	$.farbtastic(placeholder,input);
-  });
+	var selection_start = cm.getCursor(true);
+	var selection_end   = cm.getCursor(false);
+
+	var widget_div = $('<div class="change_color"><form><input type="text" id="color'+colorpickernum+'" name="color" value="'+cm.getSelection()+'" /></form><div class="colorpicker" id="colorpicker'+colorpickernum+'"></div></div>')[0]
+	var widget          = cm.addWidget(selection_start, widget_div);
+
+	$(".change_color").find('input[type=text]').focus();
+	var farb = $.farbtastic($(".colorpicker"), $(".change_color").find('input[type=text]'))
+
+	$(".change_color").focusout(function() {
+		cm.replaceRange(farb.color, selection_start, selection_end);
+		(this).remove();
+		console.log("Hello!");
+	});
 }
