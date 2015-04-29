e7c7d8a45d888eb972bed0378e81b9362836c1f1
marc <marc@openendedgroup.com>
2015-04-29 10:52:29 -0500

    added another quoting option

--- fielded/src/fielded/TextUtils.java
+++ fielded/src/fielded/TextUtils.java
@@ -15,6 +15,15 @@ public class TextUtils {
 		return JSONObject.quote(s);
 	}
 
+	/**
+	 * like quote, but without the leading and trailing " characters
+	 */
+	static public String quoteNoOuter(String s)
+	{
+		String q = JSONObject.quote(s);
+		return q.substring(1, q.length()-1);
+	}
+
 
 	/**
 	 * make a string safe for inclusion in HTML
