f9e6b69a142dd45ee03ebc6bcbc054a96c18a129
marc <marc@openendedgroup.com>
2015-04-29 10:42:03 -0500

    Better NPE behavior

--- fieldbox/src/fieldbox/boxes/plugins/DispatchBox.java
+++ fieldbox/src/fieldbox/boxes/plugins/DispatchBox.java
@@ -20,23 +20,26 @@ import static fieldbox.boxes.FLineDrawing.frameDrawing;
  * A Box with a custom drawer for holding onto and drawning a connection between two other boxes.
  * <p>
  * Automatically deletes itself should either of its two ends disappears.
- *
+ * <p>
  * head() is the parent of tail() is the dispatch graph
  */
-public class DispatchBox extends Box
-	    implements IO.Loaded // the drawer is initialized after all the properties are loaded
+public class DispatchBox extends Box implements IO.Loaded // the drawer is initialized after all the properties are loaded
 {
 
-	static private final boolean notForInsert = true; // tell FileBrowser not to bother offering us for insertion
-
-	static public final Dict.Prop<BoxRef> head = new Dict.Prop<>("head").type().toCannon().doc("the head of this topology arrow box");
-	static public final Dict.Prop<BoxRef> tail = new Dict.Prop<>("tail").type().toCannon().doc("the tail of this topology arrow box");
-
+	static public final Dict.Prop<BoxRef> head = new Dict.Prop<>("head").type()
+									    .toCannon()
+									    .doc("the head of this topology arrow box");
+	static public final Dict.Prop<BoxRef> tail = new Dict.Prop<>("tail").type()
+									    .toCannon()
+									    .doc("the tail of this topology arrow box");
 	static {
 		// these properties need to be saved in our document
 		IO.persist(head);
 		IO.persist(tail);
 	}
+	static private final boolean notForInsert = true; // tell FileBrowser not to bother offering us for insertion
+	Rect cache_h = null;
+	Rect cache_t = null;
 
 	public DispatchBox(Box head, Box tail) {
 		this.properties.put(DispatchBox.head, new BoxRef(head));
@@ -44,73 +47,12 @@ public class DispatchBox extends Box
 		loaded();
 	}
 
+
 	/**
 	 * custom constructor, call init after you are done
 	 */
 	protected DispatchBox() {
 	}
-
-	public void loaded() {
-
-		if (head()==null || tail()==null) throw new RuntimeException("can't load");
-
-		this.properties.put(Box.decorative, true);
-		this.properties.putToMap(Boxes.insideRunLoop, "main.__checkfordeletion__", this::checkForDeletion);
-		this.properties.putToMap(Boxes.insideRunLoop, "main.__updateframe__", this::updateFrameToMiddle);
-		this.properties.computeIfAbsent(frameDrawing, this::defaultdrawsLines);
-		this.properties.put(frame, head().properties.get(frame)
-							    .union(tail().properties.get(frame)));
-		this.properties.put(FrameManipulation.lockHeight, true); // the dimensions of this box cannot be changed
-		this.properties.put(FrameManipulation.lockWidth, true);
-		this.properties.put(FrameManipulation.lockX, true); // nor can its position
-		this.properties.put(FrameManipulation.lockY, true);
-
-		this.properties.put(Box.name, head().properties.get(Box.name) + "->" + tail().properties.get(Box.name));
-		this.properties.putToMap(Callbacks.onDelete, "__cleanup__", (x) -> {
-			if (x==this)
-			{
-				if (head()!=null && tail()!=null)
-					head().disconnect(tail());
-			}
-			return null;
-		});
-	}
-
-
-	protected Map<String, Function<Box, FLine>> defaultdrawsLines(Dict.Prop<Map<String, Function<Box, FLine>>> k) {
-		Map<String, Function<Box, FLine>> r = new LinkedHashMap<>();
-
-		r.put("__middle_nub__", new Cached<Box, Object, FLine>((box, previously) -> {
-			Rect rect = box.properties.get(frame);
-			if (rect == null) return null;
-			boolean selected = box.properties.isTrue(Mouse.isSelected, false);
-
-			Pair<FLine, Vec2> fa = arc(((DispatchBox) box).head().properties.get(frame), ((DispatchBox) box).tail().properties.get(frame), selected);
-
-			Vec2 at = fa.second;
-			FLine f = fa.first;
-
-			float w = selected ? 10 : 5;
-
-//			if (selected) f.circle(at.x, at.y, w);
-
-
-			Log.log("nub", "middle is "+at);
-
-			f.attributes.put(strokeColor, selected ? new Vec4(1, 1, 1, -0.1f) : new Vec4(0, 0, 0, 0.1f));
-			if (selected) f.attributes.put(thicken, new BasicStroke(selected ? 16 : 0.5f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER));
-
-//			f.attributes.put(filled, true);
-			f.attributes.put(stroked, true);
-
-			return f;
-		}, (box) -> new Triple<Boolean, Rect, Rect>(properties.get(Mouse.isSelected), ((DispatchBox) box).head().properties.get(frame), (((DispatchBox) box).tail().properties.get(frame)))));
-
-		return r;
-	}
-
-
-
 
 	static public Pair<FLine, Vec2> arc(Rect from, Rect to, boolean selected) {
 
@@ -137,6 +79,67 @@ public class DispatchBox extends Box
 		return f;*/
 	}
 
+	public void loaded() {
+
+		if (head() == null || tail() == null) throw new RuntimeException("can't load");
+
+		this.properties.put(Box.decorative, true);
+		this.properties.putToMap(Boxes.insideRunLoop, "main.__checkfordeletion__", this::checkForDeletion);
+		this.properties.putToMap(Boxes.insideRunLoop, "main.__updateframe__", this::updateFrameToMiddle);
+		this.properties.computeIfAbsent(frameDrawing, this::defaultdrawsLines);
+		this.properties.put(frame, head().properties.get(frame)
+							    .union(tail().properties.get(frame)));
+		this.properties.put(FrameManipulation.lockHeight, true); // the dimensions of this box cannot be changed
+		this.properties.put(FrameManipulation.lockWidth, true);
+		this.properties.put(FrameManipulation.lockX, true); // nor can its position
+		this.properties.put(FrameManipulation.lockY, true);
+
+		this.properties.put(Box.name, head().properties.get(Box.name) + "->" + tail().properties.get(Box.name));
+		this.properties.putToMap(Callbacks.onDelete, "__cleanup__", (x) -> {
+			if (x == this) {
+				if (head() != null && tail() != null) head().disconnect(tail());
+			}
+			return null;
+		});
+	}
+
+	protected Map<String, Function<Box, FLine>> defaultdrawsLines(Dict.Prop<Map<String, Function<Box, FLine>>> k) {
+		Map<String, Function<Box, FLine>> r = new LinkedHashMap<>();
+
+		r.put("__middle_nub__", new Cached<Box, Object, FLine>((box, previously) -> {
+			Rect rect = box.properties.get(frame);
+			if (rect == null) return null;
+			boolean selected = box.properties.isTrue(Mouse.isSelected, false);
+
+			Rect r1 = ((DispatchBox) box).head().properties.get(frame);
+			Rect r2 = ((DispatchBox) box).tail().properties.get(frame);
+
+			if (r1==null || r2==null) return null;
+
+			Pair<FLine, Vec2> fa = arc(r1, r2, selected);
+
+			Vec2 at = fa.second;
+			FLine f = fa.first;
+
+			float w = selected ? 10 : 5;
+
+//			if (selected) f.circle(at.x, at.y, w);
+
+
+			Log.log("nub", "middle is " + at);
+
+			f.attributes.put(strokeColor, selected ? new Vec4(1, 1, 1, -0.1f) : new Vec4(0, 0, 0, 0.1f));
+			if (selected) f.attributes.put(thicken, new BasicStroke(selected ? 16 : 0.5f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER));
+
+//			f.attributes.put(filled, true);
+			f.attributes.put(stroked, true);
+
+			return f;
+		}, (box) -> new Triple<Boolean, Rect, Rect>(properties.get(Mouse.isSelected), ((DispatchBox) box).head().properties.get(frame), (((DispatchBox) box).tail().properties.get(frame)))));
+
+		return r;
+	}
+
 	public Vec2 middleOf(FLine f) {
 		FLine.MoveTo m = (FLine.MoveTo) f.nodes.get(0);
 		FLine.CubicTo c = (FLine.CubicTo) f.nodes.get(f.nodes.size() - 1);
@@ -145,20 +148,22 @@ public class DispatchBox extends Box
 	}
 
 	protected boolean checkForDeletion() {
-		if (head()==null || tail()==null)
-		{
+		if (head() == null || tail() == null) {
 			Drawing.dirty(this);
 			Callbacks.delete(this);
 			this.disconnectFromAll();
 		}
 
-		if (head().parents().size() == 0 || tail().parents().size() == 0) {
+		if (head().parents()
+			  .size() == 0 || tail().parents()
+						.size() == 0) {
 			Drawing.dirty(this);
 			Callbacks.delete(this);
 			this.disconnectFromAll();
 		}
 
-		if (!head().children().contains(tail())) {
+		if (!head().children()
+			   .contains(tail())) {
 			Drawing.dirty(this);
 			Callbacks.delete(this);
 			this.disconnectFromAll();
@@ -167,16 +172,16 @@ public class DispatchBox extends Box
 		return true;
 	}
 
-
-
-	Rect cache_h = null;
-	Rect cache_t = null;
-
 	protected boolean updateFrameToMiddle() {
+		if (head() == null) return false;
+		if (tail() == null) return false;
+
 		Rect h = head().properties.get(frame);
 		Rect t = tail().properties.get(frame);
 
-		if (Util.safeEq(h,cache_h) && Util.safeEq(t, cache_t)) return true;
+		if (h == null || t == null) return false;
+
+		if (Util.safeEq(h, cache_h) && Util.safeEq(t, cache_t)) return true;
 
 		Vec2 m = arc(h, t, false).second;
 
@@ -199,9 +204,8 @@ public class DispatchBox extends Box
 	protected Box head() {
 
 		Box h = properties.get(head)
-				    .get(this);
-		if (h==null)
-		{
+				  .get(this);
+		if (h == null) {
 			Log.log("huh", "failed to find head");
 		}
 		return h;
@@ -209,10 +213,10 @@ public class DispatchBox extends Box
 
 	protected Box tail() {
 
-		Box h = properties.get(tail).get(this);
+		Box h = properties.get(tail)
+				  .get(this);
 
-		if (h==null)
-		{
+		if (h == null) {
 			Log.log("huh", "failed to find tail");
 		}
 		return h;
