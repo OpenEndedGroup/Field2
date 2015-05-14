6fa9bf18efb175b83e3f38d140da83a9d075475d
marc <marc@openendedgroup.com>
2015-05-14 16:38:02 -0500

    removed debug code

--- src/field/graphics/Shader.java
+++ src/field/graphics/Shader.java
@@ -358,7 +358,6 @@ public class Shader extends BaseScene<Shader.State> implements Scene.Perform, Li
 	@Override
 	@HiddenInAutocomplete
 	public Object asMap_set(String p, Object o) {
-		System.out.println(" asmap_set :"+o+" "+(o!=null ? o.getClass() : ""));
 		Object fo = Conversions.convert(o, Supplier.class);
 		if (fo instanceof Supplier) return getDefaultBundle().set(p, (Supplier) fo);
 		if (fo instanceof InvocationHandler) {
