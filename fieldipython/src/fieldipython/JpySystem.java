286e4b0a1210e4af9c026231568b3cf8d36fd9bf
marc <marc@openendedgroup.com>
2015-06-04 10:12:17 -0500

    FieldIPythonJPY

---
+++ fieldipython/src/fieldipython/JpySystem.java
@@ -0,0 +1,136 @@
+package fieldipython;
+
+import field.app.RunLoop;
+import fieldbox.boxes.Box;
+import fieldlinker.Linker;
+import fieldnashorn.annotations.HiddenInAutocomplete;
+import org.jpy.PyLib;
+import org.jpy.PyModule;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by marc on 5/17/15.
+ */
+public class JpySystem implements Linker.AsMap {
+
+	private final Thread thread;
+	private final Box readFrom;
+	private final Box writeTo;
+	public HashMap<String, Buffer> named = new HashMap<>();
+
+	public JpySystem(Box readFrom, Box writeTo) throws ExecutionException, InterruptedException {
+		this.readFrom = readFrom;
+		this.writeTo = writeTo;
+		CompletableFuture f = new CompletableFuture();
+		thread = new Thread(() -> {
+			PyLib.startPython();
+			PyLib.execScript("import IPython");
+			f.complete(true);
+
+			PyModule mainModule = PyModule.importModule("__main__");
+			mainModule.setAttribute("__field__", JpySystem.this);
+
+			PyLib.execScript("IPython.embed_kernel()");
+		});
+		thread.start();
+		f.get();
+
+		// embed_kernel never returns, but we do need it to run
+		Thread.sleep(1000);
+
+		RunLoop.main.onExit(thread::stop);
+	}
+
+
+	@HiddenInAutocomplete
+	public ByteBuffer name(String name, long ptr, int elements, String typedesc) {
+		try {
+
+			int len = Integer.parseInt("" + typedesc.charAt(typedesc.length() - 1));
+			ByteBuffer res = UnsafeBuffers.direct(ptr, elements * len);
+			res.order(typedesc.charAt(0) == '<' ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+
+			Buffer o = null;
+			switch (typedesc.charAt(1)) {
+				case 'f':
+					if (len == 8) o = res.asDoubleBuffer();
+					else if (len == 4) o = res.asFloatBuffer();
+					break;
+				case 'i':
+					if (len == 8) o = res.asLongBuffer();
+					else if (len == 4) o = res.asIntBuffer();
+					else if (len == 2) o = res.asShortBuffer();
+					break;
+			}
+
+			if (o==null)
+				throw new IllegalArgumentException(" can't parse __array_interface__ desc :"+typedesc);
+
+			named.put(name, o);
+			writeTo.asMap_set(name, o);
+			return res;
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	@Override
+	public boolean asMap_isProperty(String p) {
+		return named.containsKey(p);
+	}
+
+	@Override
+	public Object asMap_call(Object a, Object b) {
+		return null;
+	}
+
+	@Override
+	public Object asMap_get(String p) {
+
+		Buffer q = named.get(p);
+		if (q==null)
+			return readFrom.asMap_get(p);
+		else
+			return q;
+	}
+
+	@Override
+	public Object asMap_set(String p, Object o) {
+		return writeTo.asMap_set(p, o);
+	}
+
+	@Override
+	public Object asMap_new(Object a) {
+		return null;
+	}
+
+	@Override
+	public Object asMap_new(Object a, Object b) {
+		return null;
+	}
+
+	@Override
+	public Object asMap_getElement(int element) {
+		return null;
+	}
+
+	@Override
+	public Object asMap_setElement(int element, Object o) {
+		return null;
+	}
+
+	@Override
+	public boolean asMap_delete(Object o) {
+		return false;
+	}
+}
---
+++ fieldipython/src/fieldipython/UnsafeBuffers.java
@@ -0,0 +1,38 @@
+package fieldipython;
+
+import java.lang.reflect.Field;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+/**
+ * Utilties to go from raw 'c' pointers to Direct ByteBuffers. Code, that is, you'll go to hell for.
+ */
+public class UnsafeBuffers {
+
+
+	static public ByteBuffer direct(long pointer, int size) throws NoSuchFieldException, IllegalAccessException {
+		// if our fake bytebuffers ever get GC'ed their cleaner will free this memory, not the actual pointer
+		ByteBuffer ret = ByteBuffer.allocateDirect(0);
+
+		setLong(ret, Buffer.class, "address", pointer);
+		setInt(ret, Buffer.class, "limit", (int) size);
+		setInt(ret, Buffer.class, "capacity", (int) size);
+
+		return ret;
+	}
+
+	static private void setLong(Object ret, Class<Buffer> inside, String field, long value) throws NoSuchFieldException, IllegalAccessException {
+
+		Field f = inside.getDeclaredField(field);
+		f.setAccessible(true);
+		f.setLong(ret, value);
+
+	}
+	static private void setInt(Object ret, Class<Buffer> inside, String field, int value) throws NoSuchFieldException, IllegalAccessException {
+
+		Field f = inside.getDeclaredField(field);
+		f.setAccessible(true);
+		f.setInt(ret, value);
+	}
+
+}
