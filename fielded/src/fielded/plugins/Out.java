9dc7a80661cdbd6b93fe75642e39c4e98c47e587
marc <marc@openendedgroup.com>
2015-09-30 17:44:42 -0500

    Elide repeated type tags in output

--- fielded/src/fielded/plugins/Out.java
+++ fielded/src/fielded/plugins/Out.java
@@ -8,10 +8,13 @@ import fielded.boxbrowser.ObjectToHTML;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -45,7 +48,40 @@ public class Out extends Box {
 
 		// setup some defaults
 
-		output.map._put("Object", x -> "{HTML}<div class='maptable-entry'><b>" + cheapSynax(x.toString()) + "</b> <span class='smaller'>" + shorten(x.getClass()) + "</span></div>");
+		output.map._put("Object", x -> {
+
+			String groupName = "o"+output.joinContext();
+
+			return "{HTML}<div class='maptable-entry'><b>[[__"+groupName+"__:"+safe(x.toString())+" "+cheapSynax(x.toString()) + " ]]</b>[[__"+groupName+"smaller__:"+shorten(x.getClass())+" <span class='smaller'>" + shorten(x.getClass()) + "</span> ]]</div>";
+		}); output.map._put("java_util_List", x -> {
+			String s = "{HTML}";
+			if (((List) x).size() == 0) {
+				s += "<table class='maptable' cellspacing=0>";
+				s += "<i>Empty List of class " + shorten(x.getClass()) + "</i>";
+				s += "</table>";
+				return s;
+			}
+			List k = ((List) x);
+
+			s += "<div class='maptable' cellspacing=0><div class='smaller-inframe'>" + shorten(x.getClass()) + ", length " + k.size() + "</div>";
+			int num = 0;
+			for (Object oo : k) {
+//				s += "<div class='maptable-entry'> <div class='maptable-key'>" + num + "</div><div class='maptable-value'>" + output.convert(oo) + "</div></div>";
+				s += "<div class='maptable-entry'> <div class='maptable-value'>" + output.convert(oo, "value") + "</div></div>";
+				num++;
+				if (num > 10 && k.size() > 15) {
+					s += "<div class='maptable-entry'><div class='maptable-key'> ... </div> <div class='maptable-value'> " + num + "/" + k.size() + " total </div></div>";
+					break;
+				}
+
+			}
+			s += "</div>";
+
+			System.out.println("S ||" + s);
+
+			return s;
+		});
+
 		output.map._put("Java_Util_Map", x -> {
 			String s = "{HTML}";
 
@@ -57,40 +93,44 @@ public class Out extends Box {
 			}
 			Set k = ((Map) x).keySet();
 
-			s += "<div class='maptable' cellspacing=0><div class='smaller-inframe'>" + shorten(x.getClass()) + ", length " + k.size()+"</div>";
+			s += "<div class='maptable' cellspacing=0><div class='smaller-inframe'>" + shorten(x.getClass()) + ", length " + k.size() + "</div>";
 			int num = 0;
 			for (Object oo : k) {
-				s += "<div class='maptable-entry'> <div class='maptable-key'>" + output.convert(oo) + "</div><div class='maptable-value'>" + output.convert(((Map) x).get(oo)) + "</div></div>";
+				s += "<div class='maptable-entry'> <div class='maptable-key'>" + output.convert(oo, "key") + "</div><div class='maptable-value'>" + output.convert(((Map) x).get(oo),
+																						   "value") + "</div></div>";
 				num++;
-				if (num>10 && k.size()>15) {
-					s += "<div class='maptable-entry'><div class='maptable-key'> ... </div> <div class='maptable-value'> "+num+"/"+k.size()+" total </div></div>";
+				if (num > 10 && k.size() > 15) {
+					s += "<div class='maptable-entry'><div class='maptable-key'> ... </div> <div class='maptable-value'> " + num + "/" + k.size() + " total </div></div>";
 					break;
 				}
 
 			}
 			s += "</div>";
 
-			System.out.println("S ||"+s);
+			System.out.println("S ||" + s);
 
 			return s;
 		});
 	}
 
+	private String safe(String s) {
+		return s.replaceAll(" ", "_");
+	}
+
 	Pattern stuff = Pattern.compile("([+-]?(\\d+\\.)?\\d+)|([\\%\\$\\#\\@\\!\\^\\&\\(\\)\\[\\]\\{\\}\\'\\,\\.\\;\\:\\+\\-\\*])");
+
 	private String cheapSynax(String s) {
 
-		s = stuff.matcher(s).replaceAll((x) -> {
-			String found = x.group(1);
-			if (found!=null && found.length()>0)
-				return "<span class='number'>"+found+"</span>";
-			else
-			{
-				found = x.group(3);
-				if (found!=null && found.length()>0)
-					return "<span class='operator'>"+found+"</span>";
-			}
-			return x.group(0);
-		});
+		s = stuff.matcher(s)
+			 .replaceAll((x) -> {
+				 String found = x.group(1);
+				 if (found != null && found.length() > 0) return "<span class='number'>" + found + "</span>";
+				 else {
+					 found = x.group(3);
+					 if (found != null && found.length() > 0) return "<span class='operator'>" + found + "</span>";
+				 }
+				 return x.group(0);
+			 });
 
 
 		return s;
@@ -110,7 +150,41 @@ public class Out extends Box {
 	}
 
 	public String convert(Object x) {
-		return output.convert(x);
+		return elideGroups(output.convert(x));
 	}
 
+
+	private String elideGroups(String s) {
+		// replace subsequent sequential [[__groupName__:value foo foo]] with nothing; otherwise replace it with "foo foo"
+		// we don't support nesting here
+
+		boolean found = false;
+		Map<String, String> prev = new LinkedHashMap<>();
+
+		do {
+			Pattern p = Pattern.compile("\\[\\[__(.+?)__:(.+?) (.+?)\\]\\]");
+			Matcher m = p.matcher(s);
+			if (m.find()) {
+				String groupName = m.group(1);
+				String groupValue = m.group(2);
+				String groupPayload = m.group(3);
+
+
+				if (!prev.containsKey(groupName) || !prev.get(groupName)
+									 .equals(groupValue)) {
+					prev.put(groupName, groupValue);
+					s = s.substring(0, m.start() ) + groupPayload + s.substring(m.end() );
+					continue;
+				} else {
+					prev.put(groupName, groupValue);
+					s = s.substring(0, m.start() ) + s.substring(m.end() );
+					continue;
+				}
+			}
+			break;
+		} while (true);
+
+		return s;
+
+	}
 }
