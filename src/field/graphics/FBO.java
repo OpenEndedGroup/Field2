02b283823a10f57481531fd7267fef62e0395f2f
marc <marc@openendedgroup.com>
2015-11-27 22:02:40 -0600

    fbo contents available as an image in BoxBrowser

--- src/field/graphics/FBO.java
+++ src/field/graphics/FBO.java
@@ -1,14 +1,24 @@
 package field.graphics;
 
+import field.app.RunLoop;
+import field.utility.Dict;
 import field.utility.Log;
 import field.utility.Util;
+import fieldbox.boxes.Box;
+import fieldbox.boxes.Drawing;
+import fielded.boxbrowser.BoxBrowser;
 import fieldnashorn.annotations.HiddenInAutocomplete;
+import fieldnashorn.sourcemap.Base64;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.file.Files;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -28,7 +38,7 @@ import static org.lwjgl.opengl.GL32.glFramebufferTexture;
  * OpenGL enums, we'll grow these as necessary). They can have multiple layers, optional depth buffers, optional stencils, multisampling, a variety of components and bit-depths and dimensions.
  * <p>
  */
-public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUniform<Integer> {
+public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUniform<Integer>, BoxBrowser.HasMarkdownInformation {
 
 
 	@Override
@@ -37,6 +47,66 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 		try (Util.ExceptionlessAutoCloasable st = GraphicsContext.getContext().stateTracker.save()) {
 			return specification.unit;
 		}
+	}
+
+	int boundCount = 0;
+
+	@Override
+	public String generateMarkdown(Box inside, Dict.Prop property) {
+		Future<ByteBuffer> m = asyncDebugDownloadRGB8(null);
+
+		// repaint main window, just in case we're there
+		RunLoop.main.once(() -> {
+			Drawing.dirty(inside);
+		});
+
+		String pre
+			    = "Framebuffer object has dimensions <b>" + specification.width + "</b>x<b>" + specification.height + "</b> and is bound to texture unit <b>" + specification.unit + "</b><br>";
+		if (specification.multisample) pre += "Multisampling is turned on; ";
+		if (specification.depth) pre += "There is a depth render-buffer associated with this framebuffer; ";
+		if (specification.internalFormat == GL_RGBA32F) pre += "This is a floating-point resolution buffer; ";
+		if (specification.layers != 1) pre += "This is a multi-layer (<b>" + specification.layers + "</b> layers) buffer; ";
+		pre
+			    += "It has drawn its scene <b>" + warnIfZero(drawCount) + "</b> time" + (drawCount == 1 ? "" : "s") + (drawCount == 0 ? "</b>(are you sure .draw() is being called)" : "") + " and it has been bound as a texture <b>" + warnIfZero(boundCount) + "</b> time" + (boundCount == 1 ? "" : "s") + "<br>";
+
+		int tries = 0;
+		while (!m.isDone()) {
+			try {
+				Thread.sleep(100 * tries);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (++tries > 5) return pre + "<b>Image data for FBO is not available (FBO must be actively repainting)<b>";
+		}
+
+		try {
+
+			File tmp = File.createTempFile("field", ".jpg");
+			new FastJPEG().compress(tmp.getAbsolutePath(), m.get(), specification.width, specification.height);
+			byte[] bytes = Files.readAllBytes(tmp.toPath());
+			java.util.Base64.Encoder e = java.util.Base64.getEncoder();
+			String v = e.encodeToString(bytes);
+
+			String uri = "data:image/jpg;base64," + v;
+			String d = "<b>Snapshot of contents &mdash;<br><img style='max-width:300px' src='" + uri + "'/></b>";
+
+			return pre + d;
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "<b>Image data for FBO is not available (there was an error fetching it)<b>";
+
+	}
+
+	private String warnIfZero(int x) {
+		if (x==0) return "<span class='warning'>"+x+"</span>";
+		return ""+x;
 	}
 
 	@HiddenInAutocomplete
@@ -249,7 +319,10 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 
 	AtomicReference<Runnable> postDrawQueue = new AtomicReference<>();
 
+	int drawCount = 0;
+
 	public boolean draw() {
+		drawCount++;
 		GraphicsContext.checkError(() -> "on FBO draw entry, specification " + specification);
 		try (Util.ExceptionlessAutoCloasable st = GraphicsContext.getContext().stateTracker.save()) {
 
@@ -281,8 +354,7 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 
 			Runnable m = postDrawQueue.getAndSet(null);
 
-			if (m!= null)
-					m.run();
+			if (m != null) m.run();
 
 			GraphicsContext.checkError(() -> "on FBO draw exit1");
 
@@ -317,6 +389,8 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 
 	@Override
 	protected boolean perform0() {
+		boundCount++;
+
 		Log.log("graphics.trace", () -> "binding FBO to texture unit " + specification.unit);
 		State s = GraphicsContext.get(this);
 		for (int i = 0; i < s.text.length; i++) {
@@ -361,12 +435,10 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 			ato = ByteBuffer.allocateDirect(sz);
 		} else ato = to;
 
-		CompletableFuture<ByteBuffer> c = new CompletableFuture<ByteBuffer>()
-		{
+		CompletableFuture<ByteBuffer> c = new CompletableFuture<ByteBuffer>() {
 			@Override
 			public ByteBuffer get() throws InterruptedException, ExecutionException {
-				if (!isDone())
-					return ato;
+				if (!isDone()) return ato;
 				return super.get();
 			}
 		};
@@ -375,6 +447,44 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 			a[0] = glGetInteger(GL_FRAMEBUFFER_BINDING);
 			glBindFramebuffer(GL_FRAMEBUFFER, getOpenGLFrameBufferNameInCurrentContext());
 			glReadPixels(0, 0, specification.width, specification.height, GL11.GL_RGBA, GL_UNSIGNED_BYTE, ato);
+			glBindFramebuffer(GL_FRAMEBUFFER, a[0]);
+			c.complete(ato);
+		};
+
+		if (GraphicsContext.getContext() == null) {
+			postDrawQueue.set(r);
+		} else {
+			r.run();
+		}
+
+		return c;
+	}
+
+	/**
+	 * this operates asynchronously if we are not currently inside the draw loop (that is, if there is no valid context for our thread). only one debug download can be pending at a time. You can
+	 * pass in null to this routine and you'll get a correctly sized ByteByffer back that you can reuse for subsequent calls. Regardless of the original format of the FBO this always returns
+	 * something that's RGB / byte,.
+	 */
+	public Future<ByteBuffer> asyncDebugDownloadRGB8(ByteBuffer to) {
+		ByteBuffer ato;
+
+		int sz = specification.width * specification.height * 4;
+		if (to == null || !to.isDirect() || to.limit() < sz) {
+			ato = ByteBuffer.allocateDirect(sz);
+		} else ato = to;
+
+		CompletableFuture<ByteBuffer> c = new CompletableFuture<ByteBuffer>() {
+			@Override
+			public ByteBuffer get() throws InterruptedException, ExecutionException {
+				if (!isDone()) return ato;
+				return super.get();
+			}
+		};
+		Runnable r = () -> {
+			int[] a = {0};
+			a[0] = glGetInteger(GL_FRAMEBUFFER_BINDING);
+			glBindFramebuffer(GL_FRAMEBUFFER, getOpenGLFrameBufferNameInCurrentContext());
+			glReadPixels(0, 0, specification.width, specification.height, GL11.GL_RGB, GL_UNSIGNED_BYTE, ato);
 			glBindFramebuffer(GL_FRAMEBUFFER, a[0]);
 			c.complete(ato);
 		};
@@ -404,12 +514,10 @@ public class FBO extends BaseScene<FBO.State> implements Scene.Perform, OffersUn
 		} else ato = to;
 
 		// we hack this so that people don't hang the main thread (requiring a restart of Field) by blindly calling 'get' when isDone is false
-		CompletableFuture<FloatBuffer> c = new CompletableFuture<FloatBuffer>()
-		{
+		CompletableFuture<FloatBuffer> c = new CompletableFuture<FloatBuffer>() {
 			@Override
 			public FloatBuffer get() throws InterruptedException, ExecutionException {
-				if (!isDone())
-					return ato;
+				if (!isDone()) return ato;
 				return super.get();
 			}
 		};
