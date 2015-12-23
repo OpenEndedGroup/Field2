dcc1083773c4e994f23e8f7fd3fc0e4daea9b854
marc <marc@openendedgroup.com>
2015-12-22 21:09:43 -0600

    Refactor name of HTML information suppliers

--- fielded/src/fielded/boxbrowser/BoxBrowser.java
+++ fielded/src/fielded/boxbrowser/BoxBrowser.java
@@ -3,6 +3,7 @@ package fielded.boxbrowser;
 import field.utility.Dict;
 import field.utility.Pair;
 import fieldbox.boxes.Box;
+import fieldbox.boxes.Callbacks;
 import fieldbox.execution.Execution;
 import fieldbox.io.IO;
 import fielded.ServerSupport;
@@ -10,20 +11,15 @@ import fielded.webserver.NanoHTTPD;
 import fielded.webserver.Server;
 import org.pegdown.Extensions;
 import org.pegdown.LinkRenderer;
-import org.pegdown.Parser;
 import org.pegdown.PegDownProcessor;
-import org.pegdown.ast.AutoLinkNode;
 import org.pegdown.ast.WikiLinkNode;
-import org.pegdown.plugins.PegDownPlugins;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 /**
  * In progress
@@ -34,17 +30,20 @@ public class BoxBrowser extends Box implements IO.Loaded {
 
 	static public final String NAMED = "/named/";
 	static public final String ID = "/id/";
+
 	private final Box root;
 	private Server s;
 
 	PegDownProcessor proc = new PegDownProcessor(1000, Extensions.WIKILINKS);
 
-	public interface HasMarkdownInformation
-	{
+	public interface HasHTMLInformation {
 		String generateMarkdown(Box inside, Dict.Prop property);
 	}
 
-	static public final Dict.Prop<BiFunction<Box, Object, String>> toMarkdown= new Dict.Prop<>("toMarkdown").set(Dict.domain, "*/attributes").doc("function that takes a (box, value) and returns a markdown string that describes that box");
+	static public final Dict.Prop<BiFunction<Box, Object, String>> toMarkdown = new Dict.Prop<>("toMarkdown").set(Dict.domain, "*/attributes")
+														 .doc("function that takes a (box, value) and returns a markdown string that describes that box");
+	static public final Dict.Prop<BiFunction<Box, Object, String>> toHTML = new Dict.Prop<>("toHTML").set(Dict.domain, "*/attributes")
+														 .doc("function that takes a (box, value) and returns a HTML string that describes that box");
 
 	public BoxBrowser(Box root) {
 		this.root = root;
@@ -68,7 +67,8 @@ public class BoxBrowser extends Box implements IO.Loaded {
 				String[] pieces = uri.split("/");
 				return handleBy(x -> x.properties.has(IO.id) && x.properties.get(IO.id)
 											    .equals(pieces[0]), pieces.length > 1 ? pieces[1] : null);
-			} else return null;
+			}
+			return null;
 		});
 	}
 
@@ -76,7 +76,6 @@ public class BoxBrowser extends Box implements IO.Loaded {
 		    "<script src='/field/filesystem/codemirror-4.4/lib/codemirror.js'></script>" +
 		    "<link rel='stylesheet' href='/field/filesystem/codemirror-4.4/lib/codemirror.css'>" +
 		    "<link rel='stylesheet' href='/field/filesystem/codemirror-4.4/theme/default.css'>" +
-		    //"<link rel='stylesheet' href='/field/filesystem/field-codemirror.css'>" +
 		    "<link rel='stylesheet' href='/field/filesystem/field-boxbrowser.css' type='text/css'>" +
 		    "<script src='/field/filesystem/codemirror-4.4/mode/javascript/javascript.js'></script>" +
 		    "<script src='/field/filesystem/jquery-2.1.0.min.js'></script>" +
@@ -92,36 +91,37 @@ public class BoxBrowser extends Box implements IO.Loaded {
 
 		if (!first.isPresent()) return new NanoHTTPD.Response(NanoHTTPD.Response.Status.NOT_FOUND, null, "can't find box matching request");
 
-		if (propName!=null)
-		return handleBox(first.get(), new Dict.Prop<>(propName));
-		else
-			return handleBoxAllProperties(first.get());
+		if (propName != null) return handleBox(first.get(), new Dict.Prop<>(propName));
+		else return handleBoxAllProperties(first.get());
 	}
 
 	private NanoHTTPD.Response handleBoxAllProperties(Box box) {
 
 		LinkedHashSet<Dict.Prop> props = new LinkedHashSet<Dict.Prop>();
-		List<Pair<String,String>> sections = new ArrayList<>();
+		List<Pair<String, String>> sections = new ArrayList<>();
 
-		box.breadthFirst(upwards()).forEach(x -> {
-			for (Dict.Prop p : x.properties.getMap().keySet()) {
-				if (!props.contains(p) && hasInfo(x, p, x.properties.get(p))) {
-					sections.add(new Pair<>(p.getName(), render(x, x.properties.get(p), p)));
-					props.add(p);
-				}
+		box.breadthFirst(upwards())
+		   .forEach(x -> {
+			   for (Dict.Prop p : x.properties.getMap()
+							  .keySet()) {
+				   if (!props.contains(p) && hasInfo(x, p, x.properties.get(p))) {
+					   sections.add(new Pair<>(p.getName(), render(x, x.properties.get(p), p)));
+					   props.add(p);
+				   }
 
-				System.out.println(" checking :"+box+" / "+x+" "+x.properties.get(p)+" / "+(x.properties.get(p) instanceof HasMarkdownInformation));
+				   System.out.println(" checking :" + box + " / " + x + " " + x.properties.get(p) + " / " + (x.properties.get(p) instanceof HasHTMLInformation));
 
-				if (x==box && x.properties.get(p) instanceof HasMarkdownInformation)
-				{
-					sections.add(new Pair<>(p.getName(), render(x, x.properties.get(p), p)));
-					props.add(p);
-				}
-			}
-		});
+				   if (x == box && x.properties.get(p) instanceof HasHTMLInformation) {
+					   sections.add(new Pair<>(p.getName(), render(x, x.properties.get(p), p)));
+					   props.add(p);
+				   }
+			   }
+		   });
 
-		return new NanoHTTPD.Response(NanoHTTPD.Response.Status.OK, null,
-				       preamble+sections.stream().map(x -> "<div class='grouped'><h1 id='section_"+x.first+"'>"+"_."+x.first+"</h1><div id='section_"+x.first+"''>"+x.second+"</div></div>").reduce((a, b) -> a + "<BR>" + b).orElseGet(() -> "")+postamble);
+		return new NanoHTTPD.Response(NanoHTTPD.Response.Status.OK, null, preamble + sections.stream()
+												     .map(x -> "<div class='grouped'><h1 id='section_" + x.first + "'>" + "_." + x.first + "</h1><div id='section_" + x.first + "''>" + x.second + "</div></div>")
+												     .reduce((a, b) -> a + "<BR>" + b)
+												     .orElseGet(() -> "") + postamble);
 	}
 
 	private boolean hasInfo(Box x, Dict.Prop p, Object value) {
@@ -149,19 +149,26 @@ public class BoxBrowser extends Box implements IO.Loaded {
 		// extract comment blocks and render as markdown
 
 		if (property.getAttributes()
-		 .has(toMarkdown))
-		{
+			    .has(toMarkdown)) {
 			String md = property.getAttributes()
-					       .get(toMarkdown)
-					       .apply(box, source);
+					    .get(toMarkdown)
+					    .apply(box, source);
 
-			String html = "<p>"+toMarkdownToHTML(md)+"</p>";
+			String html = "<p>" + toMarkdownToHTML(md) + "</p>";
+			return html;
+		}
+		if (property.getAttributes()
+			    .has(toHTML)) {
+			String md = property.getAttributes()
+					    .get(toHTML)
+					    .apply(box, source);
+
+			String html =  md;
 			return html;
 		}
 
-		if (source instanceof  HasMarkdownInformation)
-		{
-			String html = "<p>"+((HasMarkdownInformation)source).generateMarkdown(box, property)+"</p>";
+		if (source instanceof HasHTMLInformation) {
+			String html = "<p>" + ((HasHTMLInformation) source).generateMarkdown(box, property) + "</p>";
 			return html;
 		}
 
@@ -169,7 +176,7 @@ public class BoxBrowser extends Box implements IO.Loaded {
 		String commentStart = "/*";
 		String commentEnd = "*/";
 
-		String[] ss = (""+source).split("\n");
+		String[] ss = ("" + source).split("\n");
 
 		List<Section> sections = new ArrayList<>();
 		Section s = new Section();
@@ -200,12 +207,12 @@ public class BoxBrowser extends Box implements IO.Loaded {
 			return "<p>nothing in property called " + proc.markdownToHtml("`" + property.getName() + "`") + "</p>";
 		}
 
-		String o = ""+sections.stream()
-				   .filter(x -> x.a.trim()
-						   .length() > 0)
-				   .map(x -> render(box, property, x))
-				   .reduce((a, b) -> (""+a) + (""+b))
-				   .get();
+		String o = "" + sections.stream()
+					.filter(x -> x.a.trim()
+							.length() > 0)
+					.map(x -> render(box, property, x))
+					.reduce((a, b) -> ("" + a) + ("" + b))
+					.get();
 
 		return o;
 	}
@@ -214,7 +221,6 @@ public class BoxBrowser extends Box implements IO.Loaded {
 
 	private String render(Box box, Dict.Prop<Object> property, Section x) {
 		if (x.comment > 0) {
-
 			String h = toMarkdownToHTML(x.a);
 
 			return "<p>" + h + "</p>";
