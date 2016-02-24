08a15fcf79af9397bfcc7276fe558109ed7ba6b9
marc <marc@openendedgroup.com>
2016-02-24 17:23:28 -0600

    More ReflectionTools for hacking through Java

--- src/field/utility/ReflectionTools.java
+++ src/field/utility/ReflectionTools.java
@@ -1,6 +1,7 @@
 package field.utility;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionTools {
 
@@ -11,6 +12,62 @@ public class ReflectionTools {
 			from = _get(from, pieces[i]);
 		}
 		return from;
+	}
+
+	static public Method getMethod(Object from, String path) throws NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+		String[] pieces = path.split("/");
+		for (int i = 0; i < pieces.length; i++) {
+			if (i < pieces.length - 1) from = _get(from, pieces[i]);
+			else return getMethod(from.getClass(), pieces[pieces.length - 1]);
+		}
+		return null;
+	}
+
+
+	public static Method getMethod(Class c, String name) throws NoSuchMethodException {
+
+//		System.out.println(" check :" + c + " for " + name+" ? ");
+
+		try {
+			Method f = c.getDeclaredMethod(name);
+			if (f != null) {
+				f.setAccessible(true);
+				return f;
+			}
+		} catch (NoSuchMethodException e) {
+		}
+
+		System.out.println(" ? ");
+		Method[] m = c.getDeclaredMethods();
+		for (Method mm : m) {
+//			System.out.println(" check :" + c + " / "+mm+" for " + name);
+			if (mm.getName()
+			      .equals(name)) {
+				mm.setAccessible(true);
+				return mm;
+			}
+		}
+
+		try {
+			Method f = c.getMethod(name);
+			if (f != null) {
+				f.setAccessible(true);
+				return f;
+			}
+		} catch (NoSuchMethodException e) {
+		}
+		Class[] inter = c.getInterfaces();
+		for (Class cc : inter) {
+			try {
+				Method o = getMethod(cc, name);
+				if (o != null) return o;
+			} catch (NoSuchMethodException e) {
+			}
+		}
+		c = c.getSuperclass();
+		if (c != null) return getMethod(c, name);
+
+		throw new NoSuchMethodException(" can't find " + name + " tried everywhere");
 	}
 
 	static protected Object _get(Object from, String name) throws IllegalAccessException, NoSuchFieldException {
@@ -38,7 +95,7 @@ public class ReflectionTools {
 		Class[] inter = c.getInterfaces();
 		for (Class cc : inter) {
 			try {
-				Object o = __get(from, c, name);
+				Object o = __get(from, cc, name);
 				if (o != null) return o;
 			} catch (NoSuchFieldException e) {
 			}
@@ -46,6 +103,6 @@ public class ReflectionTools {
 		c = c.getSuperclass();
 		if (c != null) return __get(from, c, name);
 
-		throw new NoSuchFieldException(" can't find "+name+" tried everywhere");
+		throw new NoSuchFieldException(" can't find " + name + " tried everywhere");
 	}
 }
