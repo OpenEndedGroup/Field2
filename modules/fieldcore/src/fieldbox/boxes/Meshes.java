7d9bcc50a928efb399b7f30dda64122cf3bc3def
marc <marc@openendedgroup.com>
2016-04-06 11:39:00 -0500

    add meshes directly to the box-graph

--- modules/fieldcore/src/fieldbox/boxes/Meshes.java
+++ modules/fieldcore/src/fieldbox/boxes/Meshes.java
@@ -1,7 +1,58 @@
 package fieldbox.boxes;
 
+import field.graphics.BaseMesh;
+import field.graphics.FLine;
+import field.graphics.MeshBuilder;
+import field.graphics.Scene;
+import field.utility.Dict;
+import field.utility.IdempotencyMap;
+import fieldbox.ui.FieldBoxWindow;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
 /**
- * Created by marc on 3/21/16.
+ * Allows the direct attachment of BaseMesh objects to the box-graph, and direct attachment of anything
  */
-public class Meshes {
+public class Meshes extends Box implements Drawing.Drawer {
+	static public final Dict.Prop<IdempotencyMap<Supplier<BaseMesh>>> meshes = new Dict.Prop<>("meshes").type()
+		.toCannon()
+		.doc("Geometry (specifically instances of BaseMesh) to be drawn along with this box")
+		.autoConstructs(() -> new IdempotencyMap<>(Supplier.class));
+
+	static public final Dict.Prop<Scene> scene = new Dict.Prop<>("scene").type()
+		.toCannon()
+		.doc("Scenegraph of the main layer of the main window");
+
+
+	public Meshes(Box root) {
+		this.properties.putToList(Drawing.drawers, this);
+		Optional<FieldBoxWindow> w = root.find(Boxes.window, root.both())
+			.findFirst();
+		this.properties.put(scene, w.get().getCompositor().getLayer("__main__").getScene());
+	}
+
+	@Override
+	public void draw(Drawing context) {
+		context.getShader().attach(new Scene.Transient(this::traverseAndDraw, 1).setOnceOnly());
+	}
+
+	// Exception handling!
+	protected void traverseAndDraw() {
+		this.forEach(x -> {
+			if (!x.properties.has(meshes)) return;
+			IdempotencyMap<Supplier<BaseMesh>> m = x.properties.get(meshes);
+			if (m.size() == 0) return;
+
+			m.values().stream().filter(z -> z != null).map(z -> z.get()).filter(z -> z != null).forEach(z -> {
+				draw(z);
+			});
+		});
+	}
+
+	private void draw(BaseMesh x) {
+		for (int i : x.getPasses()) {
+			x.perform(i);
+		}
+	}
 }
