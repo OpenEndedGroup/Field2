fe34161554d943bbb2104c13abadf5b1d97e5e78
marc <marc@openendedgroup.com>
2016-07-10 17:07:21 +0100

    __ synonym for _.root

--- modules/fieldcore/src/fieldnashorn/Nashorn.java
+++ modules/fieldcore/src/fieldnashorn/Nashorn.java
@@ -216,7 +216,7 @@ public class Nashorn implements BiFunction<Box, Dict.Prop<String>, NashornExecut
 
 	void setupInitialBindings(ScriptContext context, Box first) {
 		context.setAttribute("_", first, ScriptContext.ENGINE_SCOPE);
-		context.setAttribute("__", first.find(Boxes.root, first.both()), ScriptContext.ENGINE_SCOPE);
+		first.find(Boxes.root, first.both()).findFirst().ifPresent(x -> context.setAttribute("__", x, ScriptContext.ENGINE_SCOPE));
 
 		first.properties.put(boxBindings, context);
 	}
