5da8eba35ea7eb59997775abf4e848b06bf7196d
marc <marc@openendedgroup.com>
2016-07-16 22:33:12 +0200

    double-click to rename

---
+++ modules/fieldcore/src/fieldbox/boxes/plugins/DoubleClickToRename.java
@@ -0,0 +1,45 @@
+package fieldbox.boxes.plugins;
+
+import fieldbox.boxes.Box;
+import fieldbox.boxes.FrameManipulation;
+import fieldbox.boxes.Intersects;
+import fieldbox.boxes.Mouse;
+import fieldcef.plugins.GlassBrowser;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Created by marc on 7/11/16.
+ */
+public class DoubleClickToRename extends Box {
+
+	public DoubleClickToRename(Box root) {
+		this.properties.putToMap(Mouse.onDoubleClick, "__doubleClickToRename__", e -> {
+			if (e.after.keyboardState.isSuperDown()) {
+
+				Box o = Intersects.startAt(e.after, root);
+				if (o==root) return;
+				FrameManipulation.setSelectionTo(root, Collections.singleton(o));
+
+
+				this.find(GlassBrowser.glassBrowser, both()).findFirst().ifPresent(x -> {
+					x.joinCommands("Rename Box");
+				});
+
+
+//				this.find(RunCommand.runCommand, both()).findFirst().ifPresent(x -> {
+//					System.out.println(" running command ");
+////					x.apply(selected, "Rename");
+//
+//
+//				});
+			}
+		});
+	}
+
+	private Stream<Box> selection() {
+		return breadthFirst(both()).filter(x -> x.properties.isTrue(Mouse.isSelected, false));
+	}
+}
