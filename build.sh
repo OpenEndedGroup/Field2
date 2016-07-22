54f443831c319630fa3ee38be5ed63b3f81bbd4b
marc <marc@openendedgroup.com>
2016-07-22 13:41:50 +0200

    work around ant build problem

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cp -r ./lib/helperapp.app ./out/production/fieldwork2/
 
 # IDEA's ant build doesn't copy the executable bit on this app
 chmod 777 ./out/production/fieldcore/helperapp.app/Contents/Frameworks/jcef\ Helper.app/Contents/MacOS/jcef\ Helper
+chmod 777 ./out/production/fieldlinker/helperapp.app/Contents/Frameworks/jcef\ Helper.app/Contents/MacOS/jcef\ Helper
 
 # IDEA's ant build messes up the MANIFEST.MF (including the wrong one)
 jar vumf out/production/fieldagent/META-INF/MANIFEST.MF out/artifacts/fieldagent_jar/fieldagent.jar 
