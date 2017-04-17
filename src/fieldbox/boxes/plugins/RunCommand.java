a40e0969a82cf4773820f8d2f2b9e6aaea4cfb56
marc <marc@openendedgroup.com>
2017-04-17 11:02:39 -0500

    special completion on _.runCommand

--- src/fieldbox/boxes/plugins/RunCommand.java
+++ src/fieldbox/boxes/plugins/RunCommand.java
@@ -4,60 +4,80 @@ import field.utility.Dict;
 import field.utility.Log;
 import field.utility.Triple;
 import fieldbox.boxes.Box;
+import fieldbox.execution.Completion;
+import fieldbox.execution.HandlesQuoteCompletion;
 import fielded.Commands;
 import fielded.RemoteEditor;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Plugin offers _.runCommand("match") and associated functionality.
- *
+ * <p>
  * Note we return boolean as to whether matches were found and ran, but Nashorn right now just eats the return value and turns it into Undefined
- *
+ * <p>
  * We need a way of chaining these things together, but it's hard to do without access to the return value of runCommand...
  */
 public class RunCommand extends Box {
 
-	static public final Dict.Prop<BiFunctionOfBoxAnd<String, Boolean>> runCommand = new Dict.Prop<>("runCommand").doc("`_.runCommand(x)` runs commands for box `_` that match string `x`")
-															      .type()
-															      .toCannon();
+	static public final Dict.Prop<BiFunctionOfBoxAnd<String, Boolean>> runCommand
+		= new Dict.Prop<>("runCommand").doc("`_.runCommand(x)` runs commands for box `_` that match string `x`")
+		.type()
+		.toCannon();
+	private Box root;
 
 	public RunCommand(Box root) {
-		this.properties.put(runCommand, this::run);
+		this.root = root;
+		this.properties.put(runCommand, new RunCompletor());
 	}
 
-	protected boolean run(Box box, String of) {
+	protected class RunCompletor implements BiFunctionOfBoxAnd<String, Boolean>, HandlesQuoteCompletion {
 
-		Log.log("run.command",()-> box + " " + of);
+		@Override
+		public List<Completion> getQuoteCompletionsFor(String prefix) {
+			List<Triple<String, String, Runnable>> commands = Commands.getCommandsAndDocs(root);
+			return commands.stream().filter(x -> stripFormatting(x.first.toLowerCase()).startsWith(prefix.toLowerCase())).map(x -> {
+				Completion c = new Completion(-1, -1, x.first, "<span class='doc'>"+x.second+"</span>");
+				return c;
+			}).collect(Collectors.toList());
+		}
 
-		Pattern p = Pattern.compile(of);
-		List<Triple<String, String, Runnable>> commands = Commands.getCommandsAndDocs(box);
+		@Override
+		public Boolean apply(Box box, String of) {
 
-		System.out.println(" commands ? "+commands);
+			Log.log("run.command", () -> box + " " + of);
 
-		Log.log("run.command", ()->"command size is " + commands.size());
-		if (commands.size() == 0) return false;
+			Pattern p = Pattern.compile(of);
+			List<Triple<String, String, Runnable>> commands = Commands.getCommandsAndDocs(box);
 
-		// Nashorn doesn't like a lambda here
+			System.out.println(" commands ? " + commands);
 
-		boolean[] found  = {false};
+			Log.log("run.command", () -> "command size is " + commands.size());
+			if (commands.size() == 0) return false;
 
-		commands.stream().filter(x -> stripFormatting(x.first.toLowerCase()).equals(of.toLowerCase())).forEach(r -> {
-			if (r.third instanceof RemoteEditor.ExtendedCommand) {
-				((RemoteEditor.ExtendedCommand) r.third).begin((pr, o, a) -> {
-					a.begin(null, null); // SHOULD BE AN ARG
-					a.run();
+			// Nashorn doesn't like a lambda here
+
+			boolean[] found = {false};
+
+			commands.stream().filter(x -> stripFormatting(x.first.toLowerCase()).equals(of.toLowerCase())).forEach(r -> {
+				if (r.third instanceof RemoteEditor.ExtendedCommand) {
+					((RemoteEditor.ExtendedCommand) r.third).begin((pr, o, a) -> {
+						a.begin(null, null); // SHOULD BE AN ARG
+						a.run();
+						found[0] = true;
+					}, null);
+					r.third.run();
+				} else {
+					r.third.run();
 					found[0] = true;
-				}, null);
-				r.third.run();
-			} else {
-				r.third.run();
-				found[0] = true;
-			}
-		});
-		return found[0];
+				}
+			});
+			return found[0];		}
 	}
+
 
 	private String stripFormatting(String s) {
 		return s.replaceAll("<[^>]*>", "");
