80680a9fced5d1378e1a8b46be3b296af17ee7b5
marc <marc@openendedgroup.com>
2017-05-22 11:25:58 -0500

    shader preprocessor

--- src/field/utility/ShaderPreprocessor.java
+++ src/field/utility/ShaderPreprocessor.java
@@ -3,7 +3,10 @@ package field.utility;
 import fieldbox.boxes.Box;
 import fieldbox.boxes.plugins.Exec;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,9 +22,35 @@ public class ShaderPreprocessor {
 		Matcher q = p.matcher(s);
 		return q.replaceAll(x -> {
 			String g = q.group(2);
-			return ""+e.apply(inside, g).first;
+			Object z = e.apply(inside, g).first;
+			if (z == null || ("" + z).toLowerCase().equals("undefined")) {
+				return "";
+			} else return "" + z;
 		});
-
 	}
+
+
+	public class Preprocess implements Supplier<String> {
+		private final String source;
+		private String evaluatedTo;
+		private final Box inside;
+
+		public Preprocess(Box inside, String source) {
+			this.inside = inside;
+			this.source = source;
+		}
+
+		public String getSource() {
+			return source;
+		}
+
+		@Override
+		public String get() {
+			if (evaluatedTo == null)
+				return evaluatedTo = preprocess(inside, source);
+			else return evaluatedTo;
+		}
+	}
+
 
 }
