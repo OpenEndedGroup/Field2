39bca64349129cc99e2bc85c5335a41cba582cee
marc <marc@openendedgroup.com>
2017-05-22 11:22:12 -0500

    less debug

--- osx/src/fieldcef/plugins/GlassBrowser.java
+++ osx/src/fieldcef/plugins/GlassBrowser.java
@@ -245,7 +245,6 @@ public class GlassBrowser extends Box implements IO.Loaded {
 	}
 
 	public void runCommands() {
-		System.out.println(" -- running commands -- ");
 		browser.executeJavaScript("goCommands()");
 		show();
 	}
