4d13db54a051b8b6e752b5f42933f375f0f77784
marc <marc@openendedgroup.com>
2017-07-03 11:46:50 -0500

    missing file

---
+++ src/field/graphics/FLineSerializationHelper.kt
@@ -0,0 +1,50 @@
+package field.graphics
+
+import field.linalg.Vec3
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
+object FLineSerializationHelper
+{
+    @JvmStatic
+    fun writeObject(f : FLine, oos : ObjectOutputStream)
+    {
+        val i = ArrayList<Vec3>()
+
+        var instructions = "";
+        for(nn in f.nodes)
+        {
+            when(nn)
+            {
+                is FLine.CubicTo -> {
+                    instructions+="c"
+                    i += nn.c1
+                    i += nn.c2
+                    i += nn.to
+                }
+                is FLine.LineTo -> {
+                    instructions+="l"
+                    i+= nn.to
+                }
+                is FLine.MoveTo-> {
+                    instructions+="m"
+                    i+= nn.to
+                }
+            }
+        }
+
+        oos.writeObject(instructions to i)
+    }
+
+    @JvmStatic
+    fun readObject(f : FLine, ois : ObjectInputStream)
+    {
+        val p = ois.readObject()
+
+        if (p is Pair<*, *>)
+        {
+            f.data(""+p.first, p.second)
+        }
+
+    }
+}
