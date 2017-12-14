e3ea02712edc4be22208c6fe320d95d24a0c4051
marc <marc@openendedgroup.com>
2017-12-14 08:31:20 -0600

    build.sh that actually works again

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+
+JDK_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/
+
 root=`dirname $0`
 cd $root
 
@@ -11,7 +14,7 @@ function join() {
 
 if [ ! -d kotlinc ]; then
     echo -- downloading kotlinc --
-    curl -L https://github.com/JetBrains/kotlin/archive/build-1.1.2-release-105.zip > kotlinc.zip
+    curl -L https://github.com/JetBrains/kotlin/releases/download/v1.2.10/kotlin-compiler-1.2.10.zip > kotlinc.zip
     echo -- decompressing --
     unzip kotlinc.zip
     echo -- complete --
@@ -64,13 +67,21 @@ find ../src -iname '*.java' > source
 find ../osx/src -iname '*.java' >> source
 
 cp00=$(join ':' ../lib/jars/*.jar)
-cp01=$(join ':' ../osx/lib/jars/gumtree/*.jar)
+cp01=$(join ':' ../lib/jars/gumtree/*.jar)
 cp1=$(join ':' ../osx/lib/jars/*)
 
 
-# XDignore.symbol.file suppresses the otherwise unspressable warning about Unsafe, which will be there until there's a replacement for Unsafe
-../kotlinc/bin/kotlinc -jdk-home /Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/ -J--add-opens -Jjava.base/jdk.internal.misc=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED -J--add-opens -Jjava.base/java.util.concurrent.atomic=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED  -classpath field_agent.jar:field_linker.jar:$cp00:$cp01:$cp1 -d classes ../src
+echo -- 1/3 compiling Kotlin -- 
 
+# XDignore.symbol.file suppresses the otherwise unspressable warning about Unsafe, which will be there until there's a replacement for Unsafe
+../kotlinc/bin/kotlinc -jvm-target 1.8 -jdk-home $JDK_HOME -J--add-opens -Jjava.base/jdk.internal.misc=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED -J--add-opens -Jjava.base/java.util.concurrent.atomic=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED  -classpath field_agent.jar:field_linker.jar:$cp00:$cp01:$cp1 -d classes ../src ../osx/src
+
+echo -- 2/3 compiling Java -- 
 javac -Xlint:-deprecation -Xlint:-unchecked -XDignore.symbol.file -classpath "field_agent.jar:field_linker.jar:../lib/jars/*:../lib/jars/orientdb/*:../osx/lib/jars/*:classes/"  @source -d classes/
 
+echo -- 3/3 copying other sources into build directory
+rsync --prune-empty-dirs --partial --progress --archive --exclude "*.java" --exclude "*.kt" ../src/* classes/
+rsync --prune-empty-dirs --partial --progress --archive --exclude "*.java" --exclude "*.kt" ../osx/src/* classes/
+
 echo -- build complete -- 
+
