f5a6669a17870ea838cfacee2536f4da77f0f6e7
marc <marc@openendedgroup.com>
2018-01-08 11:16:37 -0600

    DocumentationProxyTo -- good for Processing support?

---
+++ src/field/utility/DocumentationProxyTo.java
@@ -0,0 +1,13 @@
+package field.utility;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DocumentationProxyTo
+{
+	Class value();
+}
