eae959864fdca4ab404171e0060af189672ce9b0
marc <marc@openendedgroup.com>
2018-03-31 21:22:27 -0500

    fixed OSX build

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ function join() {
 
 if [ ! -d kotlinc ]; then
     echo -- downloading kotlinc --
-    curl -L https://github.com/JetBrains/kotlin/releases/download/v1.2.10/kotlin-compiler-1.2.10.zip > kotlinc.zip
+    curl -L https://github.com/JetBrains/kotlin/releases/download/v1.2.31/kotlin-compiler-1.2.31.zip > kotlinc.zip
     echo -- decompressing --
     unzip kotlinc.zip
     echo -- complete --
@@ -40,7 +40,7 @@ echo
 echo -- building field agent jar --
 find ../agent/src -iname '*.java' > agent_source
 echo -- 1/2 - compiling classes --
-javac -Xlint:-deprecation -Xlint:-unchecked -classpath "../agent/lib/*"  @agent_source -d agent_classes/
+javac -source 1.9 -target 1.9 -Xlint:-deprecation -Xlint:-unchecked -classpath "../agent/lib/*"  @agent_source -d agent_classes/
 cd agent_classes
 cp -r ../../agent/lib/META-INF .
 echo -- 2/2 - making jar --
@@ -52,7 +52,7 @@ echo
 echo -- building field linker jar --
 find ../linker/src -iname '*.java' > linker_source
 echo -- 1/2 - compiling classes --
-javac -Xlint:-deprecation -Xlint:-unchecked -classpath "../linker/lib/*"  @linker_source -d linker_classes/
+javac -source 1.9 -target 1.9 -Xlint:-deprecation -Xlint:-unchecked -classpath "../linker/lib/*"  @linker_source -d linker_classes/
 cd linker_classes
 cp -r ../../linker/lib/META-INF .
 echo -- 2/2 - making jar --
@@ -77,7 +77,7 @@ echo -- 1/3 compiling Kotlin --
 ../kotlinc/bin/kotlinc -jvm-target 1.8 -jdk-home $JDK_HOME -J--add-opens -Jjava.base/jdk.internal.misc=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED -J--add-opens -Jjava.base/java.util.concurrent.atomic=ALL-UNNAMED -J--add-opens -Jjava.desktop/sun.awt=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang.reflect=ALL-UNNAMED -J--add-opens -Jjava.base/java.lang=ALL-UNNAMED -J--add-opens -Jjava.base/java.util=ALL-UNNAMED  -classpath field_agent.jar:field_linker.jar:$cp00:$cp01:$cp1 -d classes ../src ../osx/src
 
 echo -- 2/3 compiling Java -- 
-javac -Xlint:-deprecation -Xlint:-unchecked -XDignore.symbol.file --add-opens java.desktop/com.apple.eawt=ALL-UNNAMED -classpath "field_agent.jar:field_linker.jar:../lib/jars/*:../lib/jars/orientdb/*:../osx/lib/jars/*:classes/"  @source -d classes/
+javac -Xlint:-deprecation -Xlint:-unchecked -XDignore.symbol.file -source 1.9 -target 1.9 --add-opens java.desktop/com.apple.eawt=ALL-UNNAMED -classpath "field_agent.jar:field_linker.jar:../lib/jars/*:../lib/jars/orientdb/*:../osx/lib/jars/*:classes/"  @source -d classes/
 
 echo -- 3/3 copying other sources into build directory
 rsync --prune-empty-dirs --partial --progress --archive --exclude "*.java" --exclude "*.kt" ../src/* classes/
